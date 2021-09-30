d73eeba92e13bd043ff9b7a4ddb171d39a3318ed
ezidol <goodyoungmin@naver.com>
2021-09-30 15:48:05 +0900

    5.3. Method Syntax

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,6 +3,20 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.width
+    }
+    fn square(size:u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
 fn main() {
     // Listing 5-8: Calculating the area of a rectangle specified by separate width and height variables
     let width1 = 55;
@@ -31,6 +45,32 @@ fn main() {
         area3(&rect1)
     );
     println!("rect1 is {:#?}", rect1);
+    
+    //Defining an area method on the Rectangle struct
+    let rect1 = Rectangle {
+        width: 500,
+        height: 400,
+    };
+
+    println!("rect1 is {}", rect1.area());
+
+    //Methods with More Parameters
+    let rect2 = Rectangle {
+        width: 300,
+        height: 200,
+    };
+    let rect3 = Rectangle {
+        width: 600,
+        height: 400,
+    };
+
+    println!("rect1 can hold rect2 {}", rect1.can_hold(&rect2));
+    println!("rect1 can hold rect3 {}", rect1.can_hold(&rect3));
+
+    //Associated Functions
+
+    let sq1 = Rectangle::square(30);
+    println!("{:#?}", sq1);
 }   
 
 fn area1(width1: u32, height1: u32) -> u32 {
