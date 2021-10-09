5a6491e3ee6c4a603f54dce86dc043f0b5108316
ezidol <goodyoungmin@naver.com>
2021-10-10 01:22:06 +0900

    solve the problem

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,27 +1,28 @@
 fn main() {
-    for c in "안녕하세요".chars() {
-        println!("{}", c);
-    }
-    for b in "안녕하세요".bytes() {
-        println!("{}", b);
-    }
-    let s1 = String::from("Hello ");
-    let s2 = String::from("world");
-    let s3 = s1 + &s2;
-    let s4 = &s3[..];
-    println!("{}", s4);
+    let list = vec![3,4,1,2,6,7,8,9,5,33,6324,32123,222];
+    let mean = get_mean(&list);
+    let median = get_median(&list);
 
-    for c in s4.chars() {
-        println!("{}", c);
+    println!("{} {}", mean, median);
+}
+
+fn get_mean(list:&Vec<i32>) -> i32 {
+    let mut mean = 0;
+    let list_len = list.len() as i32;
+    for x in list {
+        mean += x;
     }
+    mean = mean / list_len;
+    mean
+}
 
-    let s5 = "Hello world";
-    let s6 = s5;
-    let s7 = String::from("Bye");
-    let s8 = &s7;
+fn get_median(list:&Vec<i32>) -> i32 {
+    let mut list_clone:Vec<i32> = list.clone();
+    let list_len = list.len() / 2;
 
-    println!("{}", s6);
-    println!("{}", s5);
-    println!("{}", s8);
-    println!("{}", s7);
+    list_clone.sort();
+
+    let list_sorted = list_clone;
+
+    list_sorted[list_len]    
 }
